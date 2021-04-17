b80440bca8144920e980574ec9db2957585c33dc
pojie <po.johnpaul@yahoo.com>
2021-04-17 10:36:45 +0800

    updated index file
    updated index file to read post json request

--- index.php
+++ index.php
@@ -4,24 +4,34 @@ require "bootstrap.php";
 
 use Src\Controller\KeyController;
 
-//header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
-//header("Access-Control-Allow-Methods: GET,POST,PUT");
-//header("Access-Control-Max-Age: 3600");
-//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-print($uri);
 $uri = explode( '/', $uri );
 
-print_r($uri);
+//retrieves server request method
+$requestMethod = $_SERVER["REQUEST_METHOD"];
 
 if ($uri[1] !== 'keystore') {
      header("HTTP/1.1 404 Not Found");
      exit();
 }
 
-$requestMethod = $_SERVER["REQUEST_METHOD"];
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+$jsonPost = null;
+if(!empty($json)){
+     $jsonPost = $json;
+}
 
-$key = null;
+$keyId = null;
+if(isset($uri[2])) {
+     $keyId = $uri[2];
+}
 
+$controller = new KeyController($dbConnection, $requestMethod, $keyId, $jsonPost);
+$controller->processRequest();
